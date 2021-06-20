9052b9e56d461abbae6443f1fe5bff6cc55f504a
Karmeshjain <jainkarmesh1@gmail.com>
2021-06-20 10:04:33 +0530

    Unity Project First

---
+++ Assets/Scripts/Printmessage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Printmessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hi karmesh");
+    }
+}
+
+   
